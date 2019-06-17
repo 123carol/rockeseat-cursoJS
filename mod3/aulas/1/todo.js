3937774d383328ac0d58723a0e61a31efec80a78
unknown <carolina_nuernberg@saojose.sc.senai.br>
2019-06-17 08:44:17 -0300

    modulo 3 aula 2

---
+++ mod3/aulas/1/todo.js
@@ -0,0 +1,7 @@
+var listElement = document.querySelector("#app ul");
+var InputElement = document.querySelector("#app input");
+var Buttonelement = document.querySelector("#app button");
+
+console.log(listElement);
+console.log (InputElement);
+console.log (Buttonelement);
